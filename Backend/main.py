1cbb507f39105b62b27b238c5e46ff642727be19
Jo Egner <vechema@gmail.com>
2015-11-09 20:18:27 -0600

    Started data types for datastore

--- Backend/main.py
+++ Backend/main.py
@@ -18,8 +18,46 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api.images import get_serving_url
 from google.appengine.api import files, images
 
+
 class User(ndb.Model):
     name = ndb.StringProperty()
+    phone_number = ndb.StringProperty()
+    squads = ndb.KeyProperty(repeated=True)
+    gathers_going = ndb.KeyProperty(repeated=True)
+    gathers_invited = ndb.KeyProperty(repeated=True)
+    gathers_ignored = ndb.KeyProperty(repeated=True)
+    gathers_interested = ndb.KeyProperty(repeated=True)
+
+
+class Gather(ndb.Model):
+    name = ndb.StringProperty()
+    latitude = ndb.FloatProperty()
+    longitude = ndb.FloatProperty()
+    time = ndb.DateTimeProperty()
+    description = ndb.StringProperty()
+    admins = ndb.KeyProperty(repeated=True)
+    users_going = ndb.KeyProperty(repeated=True)
+    users_invited = ndb.KeyProperty(repeated=True)
+    users_ignored = ndb.KeyProperty(repeated=True)
+    users_interested = ndb.KeyProperty(repeated=True)
+    visibility = ndb.StringProperty()
+    invite_level = ndb.StringProperty()
+    picture = ndb.BlobKeyProperty()
+
+
+class Squad(ndb.Model):
+    admins = ndb.KeyProperty(repeated=True)
+    members = ndb.KeyProperty(repeated=True)
+    name = ndb.StringProperty()
+    picture = ndb.BlobKeyProperty()
+    description = ndb.StringProperty()
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello World!')
+
 
 class mViewNearby (webapp2.RequestHandler):
     def get(self):
