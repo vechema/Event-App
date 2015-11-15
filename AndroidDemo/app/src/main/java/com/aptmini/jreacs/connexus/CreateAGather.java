564506403a4fa84a4a16ced43f9243ef40579b19
Andrew Stier <acstier@utexas.edu>
2015-11-15 15:00:40 -0600

    Wrote code for passing the CreateGather information to backend's /CreateGather.

--- AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/CreateAGather.java
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/CreateAGather.java
@@ -3,17 +3,45 @@ package com.aptmini.jreacs.connexus;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class CreateAGather extends FragmentActivity {
+    Context context = this;
+    int startYear;
+    int startMonth;
+    int startDay;
+    int startHour;
+    int startMinute;
+    int endYear;
+    int endMonth;
+    int endDay;
+    int endHour;
+    int endMinute;
+    String title;
+    String address;
+    List<String> numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +71,73 @@ public class CreateAGather extends FragmentActivity {
         }
     }
 
+    //Show the start-time picker dialog when the button is pressed
     public void showStartTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getFragmentManager(),"timePicker");
+    }
+
+    //Pass the Gather information to the backend
+    //Step 1, get the upload URL
+    private void getUploadURL(){
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        String request_url="http://gather.appspot.com/CreateGather";
+        System.out.println(request_url);
+        httpClient.get(request_url, new AsyncHttpResponseHandler() {
+            String upload_url;
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+
+                try {
+                    JSONObject jObject = new JSONObject(new String(response));
+
+                    upload_url = jObject.getString("upload_url");
+                    postToServer(upload_url);
+
+                } catch (JSONException j) {
+                    System.out.println("JSON Error");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                Log.e("Get_serving_url", "There was a problem in retrieving the url : " + e.toString());
+            }
+        });
+    }
+
+    //Pass the Gather information to the backend
+    //Step 2, input the information into the request parameters and send it to the backend.
+    private void postToServer(String upload_url){
+        RequestParams params = new RequestParams();
+        params.put("startYear",startYear);
+        params.put("startMonth",startMonth);
+        params.put("startDay", startDay);
+        params.put("startHour", startHour);
+        params.put("startMinute", startMinute);
+        params.put("endYear",endYear);
+        params.put("endMonth",endMonth);
+        params.put("endDay", endDay);
+        params.put("endHour", endHour);
+        params.put("endMinute", endMinute);
+        params.put("title", title);
+        params.put("address", address);
+        params.put("numbers", numbers);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(upload_url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                Log.w("async", "success!!!!");
+                Toast.makeText(context, "Gather Created Successfully!", Toast.LENGTH_SHORT).show();
+//                finish();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                Log.e("Posting_to_blob", "There was a problem in retrieving the url : " + e.toString());
+            }
+        });
     }
 }
 
