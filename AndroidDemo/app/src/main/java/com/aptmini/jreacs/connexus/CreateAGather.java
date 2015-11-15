fdf47248bf7e58a3a27cde04bb42948c70ddea1f
Andrew Stier <acstier@utexas.edu>
2015-11-15 00:21:23 -0600

    Wrote start time picker. Needs to be tested.

--- AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/CreateAGather.java
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/CreateAGather.java
@@ -1,9 +1,19 @@
 package com.aptmini.jreacs.connexus;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TimePicker;
 
-public class CreateAGather extends ActionBarActivity {
+import java.util.Calendar;
+
+public class CreateAGather extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +21,31 @@ public class CreateAGather extends ActionBarActivity {
         setContentView(R.layout.activity_create_a_gather);
     }
 
+    //Define the a fragment which will help us display a time picker dialog.
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            System.out.println(hourOfDay);
+            System.out.println(minute);
+        }
+    }
+
+    public void showStartTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(),"timePicker");
+    }
 }
+
