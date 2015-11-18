5c310e0fc2871e76ef0de7d700737e70770eff6b
Andrew Stier <acstier@utexas.edu>
2015-11-17 20:24:36 -0600

    Location input by the user is converted to latitude and longitude.

---
+++ AndroidDemo/app/src/main/java/com/aptmini/jreacs/connexus/GeoPoint.java
@@ -0,0 +1,13 @@
+package com.aptmini.jreacs.connexus;
+
+/**
+ * Created by Andrew on 11/17/2015.
+ */
+public class GeoPoint {
+    double lat;
+    double lng;
+    public GeoPoint(double lat, double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
